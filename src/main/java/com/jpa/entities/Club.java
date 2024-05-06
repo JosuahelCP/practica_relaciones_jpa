2ad8325f4df0ac0e1fb26541bc73a1e2dc4adc5b
JosuaCP <josuahel2920@gmail.con>
2024-05-06 17:40:35 -0600

    Version 1.0 de la practica de relaciones JPA con Spring boot

---
+++ src/main/java/com/jpa/entities/Club.java
@@ -0,0 +1,26 @@
+package com.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.List;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "clubes")
+public class Club {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+
+    @OneToOne(targetEntity = Coach.class, cascade = CascadeType.PERSIST)
+    private Coach coach;
+
+    @OneToMany(targetEntity = Player.class, fetch = FetchType.LAZY)
+    private List<Player> players;
+}
---
+++ src/main/java/com/jpa/entities/Coach.java
@@ -0,0 +1,22 @@
+package com.jpa.entities;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "entrenadores")
+public class Coach {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    @Column(name = "last_name")
+    private String lastName;
+    private Integer age;
+    private String nationality;
+}
